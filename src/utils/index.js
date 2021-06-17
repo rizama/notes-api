419c4b98e399d3277199f0797bf14fbc9cfac0dd
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-17 22:44:29 +0700

    ^ split method error response

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable object-curly-newline */
 /* eslint-disable camelcase */
+const ClientError = require('../exceptions/ClientError');
+
 const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
     id,
     title,
@@ -9,4 +11,24 @@ const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
     updatedAt: updated_at,
 });
 
-module.exports = { mapDBToModel };
+const responseError = (error, h) => {
+    if (error instanceof ClientError) {
+        const response = h.response({
+            status: 'fail',
+            message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+    }
+
+    // Server ERROR!
+    const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+    });
+    response.code(500);
+    console.error(error);
+    return response;
+};
+
+module.exports = { mapDBToModel, responseError };
