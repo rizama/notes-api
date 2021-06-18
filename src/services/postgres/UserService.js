70bf0a5a6e5a2d45925da96f1346cdf329803007
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-18 21:06:53 +0700

    ^ tambah verifikasi kredensial

--- src/services/postgres/UserService.js
+++ src/services/postgres/UserService.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UserService {
     constructor() {
@@ -39,7 +40,7 @@ class UserService {
 
         if (result.rows.length > 0) {
             throw new InvariantError(
-                'Gagal menambahkan user. Username sudah digunakan.',
+                'Gagal menambahkan user. Username sudah digunakan.'
             );
         }
     }
@@ -57,6 +58,28 @@ class UserService {
         }
 
         return result.rows[0];
+    }
+
+    async verifyUserCredential(username, password) {
+        const query = {
+            text: 'SELECT id, password FROM users WHERE username = $1',
+            values: [username],
+        };
+        const result = await this._pool.query(query);
+
+        if (!result.rowCount) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+
+        const { id, password: hashedPassword } = result.rows[0];
+
+        const match = await bcrypt.compare(password, hashedPassword);
+
+        if (!match) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+
+        return id;
     }
 }
 
