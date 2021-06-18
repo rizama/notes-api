5f33c0182a626f946600365b59d5e243f3e3d229
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-18 21:46:18 +0700

    + Token Manager and method generate Token

---
+++ src/tokenize/TokenManager.js
@@ -0,0 +1,9 @@
+const Jwt = require('@hapi/jwt');
+
+const TokenManager = {
+    generateAccessToken(payload) {
+        return Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY);
+    },
+};
+
+module.exports = TokenManager;
