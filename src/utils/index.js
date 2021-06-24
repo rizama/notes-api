7957926c73056e048ac6d4816768804d42cf7923
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-06-24 21:06:42 +0700

    ^ get username

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,13 +2,14 @@
 /* eslint-disable camelcase */
 const ClientError = require('../exceptions/ClientError');
 
-const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+const mapDBToModel = ({ id, title, body, tags, created_at, updated_at, username }) => ({
     id,
     title,
     body,
     tags,
     createdAt: created_at,
     updatedAt: updated_at,
+    username
 });
 
 const responseError = (error, h) => {
