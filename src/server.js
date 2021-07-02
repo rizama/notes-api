54d92e1152d8d7e9d8502ffc0345292497d8dce9
Rizky Sam Pratama <rizkysampratama@gmail.com>
2021-07-02 20:55:44 +0700

    ^ implement caching

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
-const path = require('path');
+// const path = require('path');
 const Inert = require('@hapi/inert');
 
 const notes = require('./api/notes');
@@ -27,9 +27,13 @@ const uploads = require('./api/uploads');
 const StorageService = require('./services/S3/StorageService');
 const UploadsValidator = require('./validator/uploads');
 
+// cache
+const CacheService = require('./services/redis/CacheService');
+
 const init = async () => {
-    const collaborationsService = new CollaborationsService();
-    const notesService = new NotesService(collaborationsService);
+    const cacheService = new CacheService();
+    const collaborationsService = new CollaborationsService(cacheService);
+    const notesService = new NotesService(collaborationsService, cacheService);
     const usersService = new UsersService();
     // local
     // const storageService = new StorageService(
